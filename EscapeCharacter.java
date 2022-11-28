38fcc4e6b19aa69c6dac4203acb4a4c0945e78f1
SartajAziz110 <119307193+SartajAziz110@users.noreply.github.com>
2022-11-28 19:02:57 +0500

    Create EscapeCharacter.java

---
+++ EscapeCharacter.java
@@ -0,0 +1,19 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class EscapeCharacter {
+    public static void main (String[]args){
+
+
+    
+ System.out.print("\t\tHell0");
+ System.out.println("\n    Sir I am Muntazir Hussain Panhwar.");
+System.out.print("\t   Student Of Computer Science !\n");
+System.out.print("\"Department OF Mathematics and 'Computer Science'.\"");
+System.out.print("\n Batch 2k22.");
+System.out.print("\r asad");
+System.out.println("\"");
+System.out.println("\\");
+System.out.println(" asad ali\b");
+System.out.println("\f ali");
+}
+}
