347340eefb41c2296e9f07c470264b282e5c46ed
SartajAziz110 <119307193+SartajAziz110@users.noreply.github.com>
2022-11-28 17:57:02 +0500

    Create Mult_Variable.java

---
+++ Mult_Variable.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Mult_Variable{
+public static void main (String []args){
+int age=30,salary=60000;
+double height=5.6,weight=60.6;
+char blood_group='A';
+System.out.println("My Name is Abu Zada!");
+System.out.println("My age is "+age);
+
+System.out.println("My Salary per Month is "+salary);
+
+System.out.println("My Height is "+height);
+
+System.out.println("My weight is "+weight);
+
+System.out.println("My Blood Group is "+blood_group);
+}
+}
