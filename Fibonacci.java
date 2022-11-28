ae85e3454f525153f2a5eb6d685adf6c385728dc
SartajAziz110 <119307193+SartajAziz110@users.noreply.github.com>
2022-11-28 19:05:14 +0500

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int  a = 0, b = 1, c,i;
+        for (i = 1; i <=13; i++) {
+            System.out.print("    " + a);
+            c = a + b;
+            a = b;
+            b = c;
+
+
+
+        }
+
+
+
+
+
+
+
+ }  
+}
